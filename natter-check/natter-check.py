f42fe122efa80e8f750e725f59b9dbf4c9c03df8
Mike Wang <mikewang000000@gmail.com>
2024-05-29 00:57:27 +0800

    chore: Update version string

--- natter-check/natter-check.py
+++ natter-check/natter-check.py
@@ -25,7 +25,7 @@ import socket
 import struct
 import codecs
 
-__version__ = "2.1.0"
+__version__ = "2.1.1-dev"
 
 
 def fix_codecs(codec_list = ["utf-8", "idna"]):
--- natter.py
+++ natter.py
@@ -34,7 +34,7 @@ import argparse
 import threading
 import subprocess
 
-__version__ = "2.1.0"
+__version__ = "2.1.1-dev"
 
 
 class Logger(object):
